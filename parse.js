388c1f8251b4cf98ceb691d58a04a8d9c31abf92
paraboul <paraboul@gmail.com>
2015-12-01 21:52:51 +0100

    wip

--- parse.js
+++ parse.js
@@ -9,3 +9,12 @@ var data = fs.readFileSync("./APE.idl", {encoding: "utf8"});
 var tree = WebIDL2.parse(data);
 
 console.log(util.inspect(tree, {colors: true, depth: 16}));
+
+
+for (var i = 0; i < tree.length; i++) {
+    var obj = tree[i];
+
+    if (obj.type == 'interface') {
+        
+    }
+}
