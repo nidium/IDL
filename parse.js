cfae0a700319c054df63059fc28aee3f40751d71
paraboul <paraboul@gmail.com>
2015-12-01 17:06:30 +0100

    import

---
+++ parse.js
@@ -0,0 +1,9 @@
+var WebIDL2 = require("webidl2");
+var fs = require('fs');
+var util = require("util");
+
+var data = fs.readFileSync("./APE.idl", {encoding: "utf8"});
+
+var tree = WebIDL2.parse(data);
+
+console.log(util.inspect(tree, {colors: true, depth: 16}));
